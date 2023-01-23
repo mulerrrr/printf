1af1b7cfde545a777d1ba12f3b074afda37639db
mulerrrr <mulerrrr4@gmail.com>
2023-01-23 13:23:54 -0800

    d

--- _print.c
+++ _print.c
@@ -13,15 +13,6 @@ int (*find_function(const char *format))(va_list)
 	code_f find_f[] = {
 		{"c", print_char},
 		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
 		{NULL, NULL}
 	};
 
